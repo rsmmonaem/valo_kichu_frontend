af64f7a4ba175f0fda7c83f95b49b0ec9ee7d114
rsmmonaem <vrkm55@gmail.com>
2026-02-03 16:35:29 +0600

    up

--- next.config.ts
+++ next.config.ts
@@ -6,15 +6,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        port: '8000',
+        protocol: 'https',
+        hostname: 'backend.valokichu.com',
         pathname: '/**',
       },
     ],
